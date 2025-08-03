654a4a950999df097a5b659c3381a306a3567dad
Aayushimewara <aayushimewara4@gmail.com>
2025-08-03 21:37:02 +0530

    add pictures

--- gif.py
+++ gif.py
@@ -1,5 +1,5 @@
 import imageio.v3 as iio
-filenames = ['team-pic1.png', 'team-pic2.png']
+filenames = ['team-pic1.png', 'team-pic2.png']#pictures
 images = [ ]
 for filename in filenames:
   images.append(iio.imread(filename))
